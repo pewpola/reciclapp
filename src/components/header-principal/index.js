d328c5aa9abd1094bce4115a6c7cc96858b0a2a4
Eduardo <eduardojor77@gmail.com>
2024-04-28 23:09:16 -0300

    created header principal

---
+++ src/components/header-principal/index.js
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-web';
+
+export default function HeaderPrincipal() {
+    
+}
