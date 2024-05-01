f812dcbd7bb1127181806c99d79bf07b9a063905
Eduardo <eduardojor77@gmail.com>
2024-05-01 17:59:07 -0300

    using scrowview

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,68 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { TextInput, TouchableOpacity } from 'react-native-web';
-import Header from '../../components/header-principal';
+import React from 'react';
+import { View, StyleSheet, ScrollView, Text } from 'react-native';
 import HeaderPrincipal from '../../components/header-principal';
 
 export default function Home() {
-    return (
-        <View style={styles.home}>
-            <HeaderPrincipal/>
-            <View style={styles.container}>
-                <Text style={styles.nomeMovel}>
-                    Mesa quebrada para reciclagem
-                </Text>
-                <Text style={styles.precoMovel}>
-                    R$ 30,00
-                </Text>
-            </View>    
+  return (
+    <View style={styles.home}>
+      <HeaderPrincipal />
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.container}>
+          <Text style={styles.nomeMovel}>
+            Mesa quebrada para reciclagem
+          </Text>
+          <Text style={styles.precoMovel}>
+            R$ 30,00
+          </Text>
         </View>
-    )
+        <View style={styles.container}>
+          <Text style={styles.nomeMovel}>
+            Cadeira quebrada para reciclagem
+          </Text>
+          <Text style={styles.precoMovel}>
+            R$ 50,00
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.nomeMovel}>
+            Cadeira quebrada para reciclagem
+          </Text>
+          <Text style={styles.precoMovel}>
+            R$ 50,00
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    home: {
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 4,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        backgroundColor: '#FFFFFF',
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 24,
-        paddingHorizontal: 20,
-        width: '100%',
-        height: '100%',
-    },
-    container: {
-        backgroundColor: '#D9D9D9',
-        position: 'relative',
-        marginRight: 86,
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 22,
-        paddingRight: 86,
-        paddingBottom: 41,
-        paddingLeft: 17,
-        // padding: 22, 18, 41, 17,
-        boxSizing: 'border-box',
-    },
-    nomeMovel: {
-        fontFamily: 'Inter',
-        fontWeight: '700',
-        fontSize: 16,
-        color: '#000000',
-        overflowWrap: 'break-word',
-    },
-    precoMovel: {
-        alignSelf: 'flex-start',
-        fontFamily: 'Inter',
-        fontWeight: '700',
-        fontSize: 30,
-        color: '#000000',
-        overflowWrap: 'break-word',
-    },
+  home: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 20,
+  },
+  scrollViewContent: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  container: {
+    backgroundColor: '#D9D9D9',
+    marginBottom: 20,
+    width: '48%',
+    padding: 22,
+    boxSizing: 'border-box',
+  },
+  nomeMovel: {
+    fontFamily: 'Inter',
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#000000',
+    overflowWrap: 'break-word',
+  },
+  precoMovel: {
+    fontFamily: 'Inter',
+    fontWeight: '700',
+    fontSize: 30,
+    color: '#000000',
+    overflowWrap: 'break-word',
+  },
 });
