9f0603a3753c4f64335e388aaf308678de8b069e
Eduardo <eduardojor77@gmail.com>
2024-05-01 16:41:41 -0300

    fixing import

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,41 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-web';
+import Header from '../../components/header-principal';
 
 export default function Home() {
     return (
         <View style={styles.home}>
-            <View style={styles.container}>
-                                
-            </View>
+            <HeaderP
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-    backgroundColor: '#00D455',
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    right: 0,
-    bottom: 0,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 11,
-    paddingTop: 11,
-    paddingRight: 24,
-    paddingBottom: 14,
-    paddingLeft: 8,
-    height: '100%',
-    boxSizing: 'border-box',
-    },
     home: {
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 4,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
         backgroundColor: '#FFFFFF',
-        display: 'flex',
-        flexDirection: 'column',
-        paddingBottom: 7,
-        width: 744,
-        boxSizing: 'border-box',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 24,
+        paddingHorizontal: 20,
+        width: '100%',
+        height: '100%',
     },
 });
