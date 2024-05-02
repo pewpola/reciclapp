1f4358219139a0e34af61cc2a73b682ade152a84
Eduardo <eduardojor77@gmail.com>
2024-05-02 00:07:23 -0300

    adding product detail page on navigation

--- src/navigation.js
+++ src/navigation.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Login from "./pages/login";
 import CreateAccount from "./pages/create-account";
 import Home from "./pages/home";
+import ProductDetail from './pages/product-detail';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,7 @@ const Navigation = () => {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="CreateAccount" component={CreateAccount} />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ProductDetail" component={ProductDetail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
