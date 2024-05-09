42735eb0359b141effdc61fc01c7b4721ac6f254
Eduardo <eduardojor77@gmail.com>
2024-05-09 15:42:30 -0300

    adding AddProduct to navigation

--- src/navigation.js
+++ src/navigation.js
@@ -4,6 +4,7 @@ import Login from "./pages/login";
 import CreateAccount from "./pages/create-account";
 import Home from "./pages/home";
 import ProductDetail from './pages/product-detail';
+import AddProduct from "./pages/add-product";
 
 const Stack = createStackNavigator();
 
@@ -15,6 +16,7 @@ const Navigation = () => {
         <Stack.Screen name="CreateAccount" component={CreateAccount} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="ProductDetail" component={ProductDetail} />
+        <Stack.Screen name="AddProduct" component={AddProduct}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
