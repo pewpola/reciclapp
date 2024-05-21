b05e05273fe6b643b375bc6efa20a2bc0ab0d267
Eduardo <eduardojor77@gmail.com>
2024-05-21 16:49:20 -0300

    AddSuccess

---
+++ src/pages/sucesso-ao-adicionar/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import HeaderPrincipal from '../../components/header-principal';
+
+export default function AddSuccess() {
+    return (
+        <View style={styles.container}>
+            <HeaderPrincipal />
+            <View style={styles.content}>
+                <Text style={styles.successText}>Móvel adicionado com sucesso!</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    successText: {
+        fontFamily: 'Inter',
+        fontWeight: '700',
+        fontSize: 20,
+        color: '#000000',
+    },
+});
