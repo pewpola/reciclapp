a8d56792b19ec5b16be6648d93c105aa83e3bbd3
pewpola <eduardojor77@gmail.com>
2024-05-28 16:40:12 -0300

    adding features

--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import HeaderPrincipal from '../../components/header-principal';
+import TabNavigator from '../../components/tab-navigator';
+import { useNavigation } from '@react-navigation/native';
 
 const ProfileScreen = () => {
+  const navigation = useNavigation();
+
+  const handleLogout = () => {
+    navigation.navigate('Login');
+  };
+
+  const navigateToEdit = () => {
+    navigation.navigate('EditarLocalizacaoScreen');
+  };
+
+  const navigateToContact = () => {
+    navigation.navigate('EditarMeiosDeContatoScreen');
+  };
+
   return (
     <View style={styles.container}>
       <HeaderPrincipal />
       <View style={styles.optionsContainer}>
-        <TouchableOpacity style={styles.optionButton}>
+        <TouchableOpacity style={styles.optionButton} onPress={navigateToEdit}>
           <Text style={styles.optionText}>Editar Localização</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.optionButton}>
+        <TouchableOpacity style={styles.optionButton} onPress={navigateToContact}>
           <Text style={styles.optionText}>Editar Meios de Contato</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.optionButton}>
           <Text style={styles.optionText}>Chamar Suporte</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.optionButton}>
+        <TouchableOpacity style={styles.optionButton} onPress={handleLogout}>
           <Text style={styles.optionText}>Sair da Conta</Text>
         </TouchableOpacity>
       </View>
+      <TabNavigator navigation={navigation}/>
     </View>
   );
 };
